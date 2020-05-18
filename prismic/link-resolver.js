eabcc9c388c99e81daae6601d19a95cff55fce66
Lorena Ramonda <lorena.ramonda@gmail.com>
2020-05-18 19:44:05 +0200

    Configure Prismic link-resolver

--- prismic/link-resolver.js
+++ prismic/link-resolver.js
@@ -1,3 +1,9 @@
 export default function(doc) {
+  // Define the url depending on the document type
+  if (doc.type === 'post') {
+    return '/blog/' + doc.uid
+  }
+
+  // Default to homepage
   return '/'
 }
