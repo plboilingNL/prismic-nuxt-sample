b3a95d14552f1c35b6f5bb8bb84cc1ada9ff7608
Marcin Wlodarski <73s2i4qq3f@privaterelay.appleid.com>
2020-08-26 01:35:01 +0200

    purge ignore slick carousel

--- components/VueSlickCarousel.vue
+++ components/VueSlickCarousel.vue
@@ -200,8 +200,8 @@
 </template>
 <script>
 import VueSlickCarousel from 'vue-slick-carousel'
-import 'vue-slick-carousel/dist/vue-slick-carousel.css'
-import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+import '~assets/scss/vendors/vue-slick-carousel.css'
+import '~assets/scss/vendors/vue-slick-carousel-theme.css'
 export default {
   components: {
     VueSlickCarousel
