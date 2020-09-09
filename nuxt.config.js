4c1546630e3438cfaa6b18dee3f07d5a85313f80
Marcin Wlodarski <marcinwlodarski@Marcins-MacBook-Pro.local>
2020-09-09 18:08:51 +0200

    nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -201,24 +201,5 @@ export default {
         }
       }
     }
-  },
-  generate: {
-    async routes() {
-      const client = PrismicJs.client('process.env.PRISMIC_URL')
-      async function fetchDocs(page = 1, routes = []) {
-        const response = await client.query('', {
-          pageSize: 100,
-          lang: '*',
-          page
-        })
-        const allRoutes = routes.concat(response.results)
-        if (response.results_size + routes.length < response.total_results_size) {
-          return fetchDocs(page + 1, allRoutes)
-        }
-        return [...new Set(allRoutes)]
-      }
-      const allRoutes = await fetchDocs()
-      return allRoutes.map(linkResolver)
-    }
   }
 }
